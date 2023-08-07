02443eebace37ad686f43a4d5d8c8fd9094ee985
tjy <jiyuan.tang@gientech.com>
2023-08-07 15:24:15 +0800

    week02

--- src/main/java/com/tjy/advanced/week02/GCLogAnalysis.java
+++ src/main/java/com/tjy/advanced/week02/GCLogAnalysis.java
@@ -6,43 +6,41 @@ import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author tjy
- * @date 2023/8/7
- * @apiNote
  */
 public class GCLogAnalysis {
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        //当前毫秒时间戳
+        // 当前毫秒时间戳
         long startMillis = System.currentTimeMillis();
-        //持续运行毫秒数
+        // 持续运行毫秒数; 可根据需要进行修改
         long timeoutMillis = TimeUnit.SECONDS.toMillis(1);
-        //结束时间戳
+        // 结束时间戳
         long endMillis = startMillis + timeoutMillis;
         LongAdder counter = new LongAdder();
         System.out.println("正在执行...");
-        //缓存一部分对象进入老年代
+        // 缓存一部分对象; 进入老年代
         int cacheSize = 2000;
-        Object[] cacheObjects = new Object[cacheSize];
-        //再此时间范围内循环
-        while (System.currentTimeMillis() < endMillis){
-            //生成垃圾对象
-            Object garbage = generateGarbage(10 * 1024);
+        Object[] cachedGarbage = new Object[cacheSize];
+        // 在此时间范围内,持续循环
+        while (System.currentTimeMillis() < endMillis) {
+            // 生成垃圾对象
+            Object garbage = generateGarbage(100 * 1024);
             counter.increment();
             int randomIndex = random.nextInt(2 * cacheSize);
-            if (randomIndex < cacheSize){
-                cacheObjects[randomIndex] = garbage;
+            if (randomIndex < cacheSize) {
+                cachedGarbage[randomIndex] = garbage;
             }
-
         }
-        System.out.println("执行结束！共生成对象次数："+counter.longValue());
+        System.out.println("执行结束!共生成对象次数:" + counter.longValue());
     }
 
+    // 生成对象
     private static Object generateGarbage(int max) {
-        int randomSize = random.nextInt();
+        int randomSize = random.nextInt(max);
         int type = randomSize % 4;
         Object result = null;
-        switch (type){
+        switch (type) {
             case 0:
                 result = new int[randomSize];
                 break;
@@ -53,16 +51,17 @@ public class GCLogAnalysis {
                 result = new double[randomSize];
                 break;
             default:
-                StringBuilder sb = new StringBuilder();
+                StringBuilder builder = new StringBuilder();
                 String randomString = "randomString-Anything";
-                while (sb.length() < randomSize){
-                    sb.append(randomString);
-                    sb.append(max);
-                    sb.append(randomSize);
+                while (builder.length() < randomSize) {
+                    builder.append(randomString);
+                    builder.append(max);
+                    builder.append(randomSize);
                 }
-                result = sb.toString();
+                result = builder.toString();
                 break;
         }
         return result;
     }
 }
+
